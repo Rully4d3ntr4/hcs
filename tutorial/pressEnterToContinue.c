5397ddc87e01ebe18baa71ab4831378f81114351
Omega Hanggara <hanggara33@gmail.com>
2013-06-05 13:17:06 +0700

    Added pressEnterToContinue

---
+++ tutorial/pressEnterToContinue.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+void PressEnterToContinue()
+	{
+		int c;
+		fflush( stdout );
+		do c = getchar(); while ((c != '\n') && (c != EOF));
+	}
+
+main()
+	{
+		printf("Hello, world!\n");
+		printf("Press enter to continue\n");
+		PressEnterToContinue();
+		printf("Welcome to my life!\n");
+		return 0;
+	}
